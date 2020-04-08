a8d936649e3f22240031df9169dc967227df1368
Jatawat Xie <jatawatsafe@hotmail.com>
2020-04-09 01:14:38 +0700

    [UPDATE] style

--- src/components/Comingsoon.vue
+++ src/components/Comingsoon.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-container class="about pb-12">
+  <v-container class="about pb-12" >
     <v-row class="justify-center pt-6">
       <img src="../../public/img/logoTSH_white.png" height="100px" />
     </v-row>
     <v-row class="justify-center pt-5">
-      <h2 class="font-weight-medium">Hop on with Us!</h2>
+      <h2 class="font-weight-medium" style="color:white">Hop on with Us !</h2>
     </v-row>
-
     <v-row>
       <v-col col="3" class="justify-center d-flex">
         <v-btn text icon outlined color="white">
@@ -21,18 +20,7 @@
     </v-row>
   </v-container>
 </template>
-<style>
-@import url("https://fonts.googleapis.com/css?family=Kanit&display=swap");
-h2 {
-  font-family: "Kanit", sans-serif;
-  font-weight: 300;
-  color: white;
-}
-p {
-  font-family: "Kanit", sans-serif;
-  font-weight: 300;
-}
-</style>
+
 <script>
 export default {
   name: "Home",
