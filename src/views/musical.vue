5f5b2ea3eac921e403292c457bb68032b325b474
Jatawat Xie <jatawatsafe@hotmail.com>
2020-04-16 21:11:35 +0700

    [UPDATE] function to loop data

--- src/views/musical.vue
+++ src/views/musical.vue
@@ -1,12 +1,13 @@
 <template>
   <v-content>
-    <v-container style="background: white; height: 700px">
+    <v-container style="background: white; height: 2000px">
       <v-row class="px-1">
         <v-col>
           <h1 class="text-center">{{musicalName}}</h1>
+          <a href="https://genius.com/Pasek-and-paul-hey-good-lookin-annotated">เพลง</a>
         </v-col>
       </v-row>
-      <v-row>
+      <!-- <v-row>
         <v-col>
           <ul v-for="{actName,datails} in musicals" :key="actName">
             <li>{{actName}}</li>
@@ -18,6 +19,34 @@
             </ul>
           </ul>
         </v-col>
+      </v-row>-->
+      <v-row>
+        <v-col>
+          <ul v-for="{actName,datails} in musicals" :key="actName">
+            <li>{{actName}}</li>
+            <ul v-for="{id,scene,subscenes} in datails" :key="id">
+              <li>Scene{{scene}}: </li>
+              <v-for v-for="{name,songs} in subscenes" :key="name">
+               <span>{{name}}</span>
+                <ul>
+                  <v-for v-for="{id, songName, character} in songs" :key="id">
+                    <li>{{songName}}..................................................{{character}}</li>
+                  </v-for>
+                </ul>
+              </v-for>
+            </ul>
+          </ul>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col-12">
+          <p v-html="venue"></p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col-12">
+        <div v-html="exampleTable"></div>
+        </v-col>
       </v-row>
     </v-container>
   </v-content>
@@ -25,12 +54,10 @@
 
 <script>
 import KingAndI from "../data/KingAndI.json"
+import DreamBox from "../data/DreamBox.json"
 export default {
   name: "Test",
   data: () => ({
-    musicals: KingAndI,
-    musicalName: "King and I"
-  })
 };
 </script>
 
